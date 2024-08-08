08d978ce447795b2110776c3837b5308889cdaad
dowon <ehdnjs8745@gmail.com>
2024-08-08 18:10:18 +0900

    add signup

--- common/urls.py
+++ common/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path, include
+from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
 
@@ -7,5 +7,6 @@ app_name = "common"
 
 urlpatterns = [
     path("login/", auth_views.LoginView.as_view(template_name="common/login.html"), name="login"),
-    path("logout", views.logout_view, name='logout'),
+    path("logout/", views.logout_view, name='logout'),
+    path("signup/", views.signup, name='signup'),
 ]
